8ee21e915c8a10ea657fe244ddc218db5ebd805c
JoseParra28 <iliftandcook@gmail.com>
2022-12-26 18:52:15 +0000

    push

---
+++ script.js
@@ -0,0 +1,11 @@
+const joseArray = ['jose', 'sara', 'peter', 'michael']
+console.log(joseArray)
+
+const jonas = {
+    firstName: 'jonas',
+    lastName: 'parra',
+    age: 2022 - 1993,
+    job: 'sales',
+    friends: ['jose', 'sara', 'peter', 'michael']
+}
+console.log(jonas)
