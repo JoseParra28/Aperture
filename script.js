0feeba9a0475e863e83085db3e632c43942ee23e
JoseParra28 <iliftandcook@gmail.com>
2023-01-06 09:42:27 +0000

    save

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
 'use strict'
-document.querySelector('.message').textContent = 'Well hello there';
-console.log(document.querySelector('.test').textContent) 
+// document.querySelector('.message').textContent = 'Well hello there';
+// console.log(document.querySelector('.test').textContent) 
 
-document.querySelector('.btn').addEventListener('click',function(){
-console.log(document.querySelector('.message').textContent = 'You clicked me!')
+// document.querySelector('.btn').addEventListener('click',function(){
+// (document.querySelector('.message').textContent = 'You clicked me!')
+// })
+
+
+// document.querySelector('#btn-ch').addEventListener('click',function(){
+//     (document.querySelector('.man').style.color = 'red')
+//     (document.querySelector('.test').style.color = 'blue')
+//     (document.querySelector('.parra').style.color = 'red')
+// })
+
+
+
+
+// document.getElementsByClassName = ('change')
+
+// function changeColor(element){
+//     let currentColor = element.style.backgroundColor;
+//     if (currentColor == "red"){
+//         onclick(element.backgroundColor = "green");
+//     // } else {
+//     //     element.backgroundColor = "blue"
+//     }}
+
+
+const menu = document.querySelector('#mobile-menu')
+const menuLinks = document.querySelector('.navbar__menu')
+
+menu.addEventListener('click',function(){
+    menu.classList.toggle('is-active')
+    menuLinks.classList.toggle('active');
 })
-document.getElementsByClassName = ('change')
-
-function changeColor(element){
-    let currentColor = element.style.backgroundColor;
-    if (currentColor == "red"){
-        onclick(element.backgroundColor = "green");
-    // } else {
-    //     element.backgroundColor = "blue"
-    }}
